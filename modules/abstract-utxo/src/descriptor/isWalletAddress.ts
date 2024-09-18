df8c4f5f94246b8cad7d5d3b85cad6c7731e8665
Otto Allmendinger <otto@bitgo.com>
2024-09-18 17:56:17 +0200

    refactor: simplify isWalletAddress for descriptor wallets
    Issue: BTC-1472

---
+++ modules/abstract-utxo/src/descriptor/isWalletAddress.ts
@@ -0,0 +1,33 @@
+import * as assert from 'assert';
+import * as utxolib from '@bitgo/utxo-lib';
+import { Descriptor } from '@bitgo/wasm-miniscript';
+import { UtxoCoinSpecific, VerifyAddressOptions } from '../abstractUtxoCoin';
+import * as t from 'io-ts';
+import { NamedDescriptor } from './NamedDescriptor';
+
+export function assertDescriptorWalletAddress(
+  network: utxolib.Network,
+  params: VerifyAddressOptions<UtxoCoinSpecific>,
+  descriptors: unknown
+): void {
+  assert(params.coinSpecific);
+  assert(t.array(NamedDescriptor).is(descriptors));
+  assert('descriptorName' in params.coinSpecific);
+  assert('descriptorChecksum' in params.coinSpecific);
+  const descriptorName = params.coinSpecific.descriptorName;
+  const descriptorChecksum = params.coinSpecific.descriptorChecksum;
+  const namedDescriptor = descriptors.find((d) => d.name === descriptorName);
+  if (!namedDescriptor) {
+    throw new Error(`Descriptor ${descriptorName} not found`);
+  }
+  const descriptor = Descriptor.fromString(namedDescriptor.value, 'derivable');
+  const checksum = descriptor.toString().slice(-8);
+  if (checksum !== descriptorChecksum) {
+    throw new Error(`Descriptor checksum mismatch: ${checksum} !== ${descriptorChecksum}`);
+  }
+  const derivedScript = Buffer.from(descriptor.atDerivationIndex(params.index).scriptPubkey());
+  const derivedAddress = utxolib.address.fromOutputScript(derivedScript, network);
+  if (params.address !== derivedAddress) {
+    throw new Error(`Address mismatch: ${params.address} !== ${derivedAddress}`);
+  }
+}
